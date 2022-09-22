858dbc635759844c8c01a2553654c60a462da7b5
smithblack-0 <85000757+smithblack-0@users.noreply.github.com>
2022-09-22 14:46:20 -0700

    Began developing tests for adaptive attention.

--- tests/test_adaptive.py
+++ tests/test_adaptive.py
@@ -3,95 +3,22 @@ import unittest
 from src.supertransformerlib import Adaptive
 
 
-
-class test_Calculate_Output_Update(unittest.TestCase):
-    """
-    Tests for the calculate update layer
-    """
-
+class test_Adaptive_Attention(unittest.TestCase):
     def test_constructor(self):
-        """Test the constructor runs at all."""
-        Adaptive.Calculate_Output_Update(10, 10)
-    def test_random(self):
-        """Test that the call runs with legal arguments"""
-        halting_probs = torch.zeros([2, 3],dtype=torch.float32)
-        state = torch.randn([2, 3, 12])
-        state_update = torch.randn([2, 1, 12])
-        updater = Adaptive.Calculate_Output_Update(12)
-        update_probs, update_res, update_output = updater(halting_probs, state, state_update)
-        print(halting_probs+update_probs)
-        print((halting_probs+update_probs).sum(dim=-2))
-        print(update_res)
-        print(update_output)
-    def test_repetition(self):
-        """Test the behavior upon repetition."""
+        """Test the constructor functions reasonably well"""
+        Adaptive.Adaptive_Attention(10, 20, 30, 5, 5, 5)
+    def test_startup(self):
+        """Test that the startup feature of the layer is able to make correct accumulators"""
 
-        halting_probs = torch.zeros([2, 3],dtype=torch.float32)
-        state = torch.randn([2, 3, 12])
-        state_update = torch.randn([2, 1, 12])
-        updater = Adaptive.Calculate_Output_Update(12)
-        update_probs, update_res, update_output = updater(halting_probs, state, state_update)
+        query = torch.zeros([10, 3, 5, 32])
+        key = torch.zeros([10, 3, 5, 64])
+        value = torch.zeros([10, 3, 5, 48])
+        layer = Adaptive.Adaptive_Attention(32, 64, 48, 5, 5, 5)
 
-        print(update_probs)
-        print(update_res)
+        accumulator = layer.start_accumulator(query, key, value)
+        self.assertTrue(accumulator.Halting_Probabilities.shape == query.shape[:-1])
+        self.assertTrue(accumulator.Residuals.shape == query.shape[:-1])
+        self.assertTrue(accumulator.Output.shape == torch.Size([*query.shape[:-1], value.shape[-1]]))
 
-        halting_probs += update_probs
-        update_probs, update_res, update_output = updater(halting_probs, state, state_update)
+    def test_
 
-        print(update_probs)
-        print(update_res)
-
-class test_state_engine(unittest.TestCase):
-    def test_get_unhalted_mesh(self):
-        """Test the get unhalted feature works properly."""
-
-        state = torch.randn([2, 2, 2, 3])
-        h_prob = torch.tensor([[[0.1, 0.3], [0.4, 0.6]],[[0.2, 0.4],[0.5,0.5]]])
-        t_prob = torch.randn([2,2])
-        h_res = torch.zeros([2])
-        t_res = torch.zeros([2])
-        output =  torch.zeros([2])
-        trashcan = torch.zeros([2])
-
-        engine = Adaptive.State_Engine(
-            state,
-            h_prob,
-            t_prob,
-            h_res,
-            t_res,
-            output,
-            trashcan
-        )
-        dim1, dim0 = engine.get_unhalted_mesh()
-        unhalted = h_prob[dim1, dim0]
-        self.assertTrue(torch.all(unhalted == torch.tensor([[0.1, 0.3],[0.2, 0.4]])))
-    def test_get_details(self):
-        """
-        Test the ability of get details to retrieve the required features
-        based on the state, filtering out irrelevant information and applying
-        required masks.
-        :return:
-        """
-
-        state = torch.randn([2, 2, 2, 3])
-        h_prob = torch.tensor([[[0.1, 0.3], [0.4, 0.6]],[[0.2, 0.4],[0.5,0.5]]])
-        t_prob = torch.tensor([[[0.4, 0.1], [0.1, 0.3]],[[0.2, 0.4],[0.5,0.5]]])
-        h_res = torch.zeros([2])
-        t_res = torch.zeros([2])
-        output =  torch.zeros([2])
-        trashcan = torch.zeros([2, 1, 3])
-
-        engine = Adaptive.State_Engine(
-            state,
-            h_prob,
-            t_prob,
-            h_res,
-            t_res,
-            output,
-            trashcan
-        )
-
-        state, halting_probs, trash_probs = engine.get_details()
-        print(state)
-        print(halting_probs)
-        print(trash_probs)
