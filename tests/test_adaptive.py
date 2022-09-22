2153095a11cca902e01842bdb4fa7844e8650d0d
smithblack-0 <85000757+smithblack-0@users.noreply.github.com>
2022-09-22 15:42:15 -0700

    Continuing development of adaptive test

--- tests/test_adaptive.py
+++ tests/test_adaptive.py
@@ -3,7 +3,7 @@ import unittest
 from src.supertransformerlib import Adaptive
 
 
-class test_Adaptive_Attention(unittest.TestCase):
+class test_Adaptive_Attention_Basics(unittest.TestCase):
     def test_constructor(self):
         """Test the constructor functions reasonably well"""
         Adaptive.Adaptive_Attention(10, 20, 30, 5, 5, 5)
@@ -20,5 +20,26 @@ class test_Adaptive_Attention(unittest.TestCase):
         self.assertTrue(accumulator.Residuals.shape == query.shape[:-1])
         self.assertTrue(accumulator.Output.shape == torch.Size([*query.shape[:-1], value.shape[-1]]))
 
-    def test_
+    def test_call_simple(self):
+        """Test if call works in the simplest of cases"""
+        #Setup
 
+        query = torch.randn([5, 32])
+        key = torch.randn([5, 32])
+        value = torch.randn([5, 32])
+        layer = Adaptive.Adaptive_Attention(32, 32, 32, 5, 6, 7)
+        accumulator = layer.start_accumulator(query, key, value)
+
+        #Call
+        new_accumulator = layer(accumulator, query, key, value)
+        self.assertTrue(torch.any(new_accumulator.Output != accumulator.Output))
+    def test_call_intermediate(self):
+        """Test if forward works in a mildly complicated situation"""
+        query = torch.randn([5, 32])
+        key = torch.randn([5, 48])
+        value = torch.randn([5, 64])
+        layer = Adaptive.Adaptive_Attention(32, 48, 64, 5, 6, 7)
+        accumulator = layer.start_accumulator(query, key, value)
+
+        new_accumulator = layer(accumulator, query, key, value)
+        self.assertTrue(torch.any(new_accumulator.Output != accumulator.Output))
