c75f64a6ee426fdc1ef4f6e2daef495f02299789
smithblack-0 <85000757+smithblack-0@users.noreply.github.com>
2022-09-28 12:27:44 -0700

    Fixing lint errors

--- src/supertransformerlib/Attention.py
+++ src/supertransformerlib/Attention.py
@@ -509,7 +509,7 @@ class LCSA(Core.KernelSpace, Core.Utility):
         query = query.transpose(-1, 0)
         content = content.transpose(-1, 0)
         query = query.unsqueeze(0).transpose(0, -2).squeeze(-2)
-        content = content.unsqueeze(0).transpose(0, -2).squeeze(-2) # (items,.=local_items, .., (parallel...),  head, embedding)
+        content = content.unsqueeze(0).transpose(0, -2).squeeze(-2)  # (items,.=local_items, .., (parallel...),  head, embedding)
 
         query = self.query_projector(query)
         key = self.key_projector(content)
