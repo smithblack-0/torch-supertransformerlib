7ac60439a2af713fa28f95cef06e7cf0a8b19862
smithblack-0 <85000757+smithblack-0@users.noreply.github.com>
2022-09-28 12:45:24 -0700

    Tests working on this end

--- tests/test_core.py
+++ tests/test_core.py
@@ -241,7 +241,7 @@ class test_KernelSpace(unittest.TestCase):
         instance.update_children(config)
         output = instance()
 
-        self.assertTrue(output.shape == torch.Size([30, 20, 30]), "problem: %s" % output.shape)
+        self.assertTrue(output.shape == torch.Size([30, 20, 30]), output.shape)
     def test_nested_kernel_updates(self):
 
 
