70027813b8e98c01125edd87d85be036588784e0
smithblack-0 <85000757+smithblack-0@users.noreply.github.com>
2022-09-28 11:39:18 -0700

    Continuing to fix lint errors

--- src/supertransformerlib/testing.py
+++
@@ -1,9 +0,0 @@
-from typing import Optional, Union, Dict, List, overload
-import torch
-from torch import nn
-from torch.nn import Module
-import dataclasses
-
-
-sample_tensor = torch.randn([10, 10, 10])
-index = torch.tensor([[[]]])
