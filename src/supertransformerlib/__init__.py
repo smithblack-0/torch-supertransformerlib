68d43a3ec9389f461f276d964d437ea1450777b5
smithblack-0 <85000757+smithblack-0@users.noreply.github.com>
2022-09-28 14:15:18 -0700

    Update version info

--- src/supertransformerlib/__init__.py
+++ src/supertransformerlib/__init__.py
@@ -9,6 +9,6 @@ __version__ = "2.0r1"
 
 from . import Glimpses # noqa
 from . import Attention # noqa
-from . import Adaptive #noqa
+from . import Adaptive # noqa
 from . import Core # noqa
 from . import Loss # noqa
