66bf17d594229d08456cc9368bafefff1b3d5dac
smithblack-0 <85000757+smithblack-0@users.noreply.github.com>
2022-09-28 14:09:52 -0700

    Update version info

--- src/supertransformerlib/__init__.py
+++ src/supertransformerlib/__init__.py
@@ -6,7 +6,9 @@ A library for fast, smart transformers.
 
 __version__ = "2.0r0"
 
+
 from . import Glimpses # noqa
 from . import Attention # noqa
+from . import Adaptive
 from . import Core # noqa
 from . import Loss # noqa
