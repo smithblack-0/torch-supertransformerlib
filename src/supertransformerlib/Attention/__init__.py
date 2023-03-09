bda9dd50aeefea3de13e874021c193fa258b7231
chris <chrisoquinn.2@gmail.com>
2023-03-09 11:20:37 -0800

    Updated the version info

--- src/supertransformerlib/Attention/__init__.py
+++
@@ -1,5 +0,0 @@
-from .feedforward import *
-from .Throwahead import *
-# from .parameter_block_attention import *
-from .multiheaded_attention import *
-from .random_access_attention_memory import *
--- src/supertransformerlib/__init__.py
+++ src/supertransformerlib/__init__.py
@@ -4,11 +4,7 @@ A library for fast, smart transformers.
 
 """
 
-__version__ = "2.0r2"
+__version__ = "2.1r0"
 
 from . import Core # noqa
-from . import Glimpses # noqa
-#from . import Basics #noqa
-#from . import Attention # noqa
-#from . import Adaptive # noqa
-#from . import Loss # noqa
+from . import Basics #noqa
